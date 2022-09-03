deb72ac3d4080ccfb8ea45ad908d686a26542a27
Hồ Gia Khánh <43257533+khanhpro9889@users.noreply.github.com>
2022-09-03 23:20:47 +0700

    Fix error path

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -3,6 +3,7 @@ import { Card, Skeleton } from "antd";
 import { FullscreenOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { productType } from "../../routers/Category/Category";
 import { Link, useNavigate } from "react-router-dom";
+import { SINGLE_PATH_NO_ID } from "../../constants/path;
 const { Meta } = Card;
 
 type propsType = {
@@ -36,7 +37,7 @@ export default function ProductItem({
         >
           <Meta
             title={
-              <Link to={`/product/${item.id}`}>
+              <Link to={`${SINGLE_PATH_NO_ID}/${item.id}`}>
                 <strong>{item.title}</strong>
               </Link>
             }
